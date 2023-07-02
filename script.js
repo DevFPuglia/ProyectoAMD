97253959b464a7a84dcbc6edbe9b8cc09a4bf501
DevFPuglia <dev.f.puglia@gmail.com>
2023-07-02 18:29:21 -0300

    se agrego/modifico codigo CSS y JS.

---
+++ script.js
@@ -0,0 +1,45 @@
+var swiper1 = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  var swiper2 = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+  let tabInputs = document.querySelectorAll(".tabInput");
+  
+  tabInputs.forEach(function (input) {
+    input.addEventListener('change', function () {
+      let id = input.ariaValueMax;
+      let thisSwiper = document.getElementById('swiper' + id);
+      thisSwiper.swiper.update();
+    });
+  });
